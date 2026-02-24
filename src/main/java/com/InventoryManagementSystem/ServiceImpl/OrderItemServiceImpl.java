15e287fc8191b49efb01dd33f6880bbfdd96cf5c
Patil Gita <pyogita568@gmail.com>
2026-02-24 15:19:19 +0530

    completed

--- src/main/java/com/InventoryManagementSystem/ServiceImpl/OrderItemServiceImpl.java
+++ src/main/java/com/InventoryManagementSystem/ServiceImpl/OrderItemServiceImpl.java
@@ -1,7 +1,13 @@
 package com.InventoryManagementSystem.ServiceImpl;
 
+import com.InventoryManagementSystem.Entity.Order;
 import com.InventoryManagementSystem.Entity.OrderItem;
+import com.InventoryManagementSystem.Entity.Product;
+import com.InventoryManagementSystem.Repository.OrderItemRepository;
+import com.InventoryManagementSystem.Repository.OrderRepository;
+import com.InventoryManagementSystem.Repository.ProductRepository;
 import com.InventoryManagementSystem.Service.OrderItemService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,9 +15,28 @@ import java.util.List;
 @Service
 public class OrderItemServiceImpl implements OrderItemService {
 
+    @Autowired
+    private OrderItemRepository orderItemRepository;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
     @Override
     public OrderItem createOrderItem(Long orderId, OrderItem orderItem) {
-        return null; // logic later
+
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new RuntimeException("Order not found"));
+
+        Product product = productRepository.findById(orderItem.getProduct().getId())
+                .orElseThrow(() -> new RuntimeException("Product not found"));
+
+        orderItem.setOrder(order);
+        orderItem.setProduct(product);
+
+        return orderItemRepository.save(orderItem);
     }
 
     @Override
@@ -21,6 +46,6 @@ public class OrderItemServiceImpl implements OrderItemService {
 
     @Override
     public void deleteOrderItem(Long id) {
-
+        orderItemRepository.deleteById(id);
     }
 }
