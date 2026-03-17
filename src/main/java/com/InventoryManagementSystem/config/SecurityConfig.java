5fd9d68278dfe3459f71a3e18a199ece889c8638
Sandip <your-email@gmail.com>
2026-03-17 12:24:03 +0530

    done

--- src/main/java/com/InventoryManagementSystem/config/SecurityConfig.java
+++ src/main/java/com/InventoryManagementSystem/config/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/customers/**").permitAll()
+                        .requestMatchers("/vendors/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
