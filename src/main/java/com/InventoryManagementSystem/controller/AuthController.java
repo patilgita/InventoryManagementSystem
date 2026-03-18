87d5f82b7f97718e26b28c667b20372190eaaaaf
Patil Gita <pyogita568@gmail.com>
2026-03-18 14:16:26 +0530

    completed

--- src/main/java/com/InventoryManagementSystem/controller/AuthController.java
+++ src/main/java/com/InventoryManagementSystem/controller/AuthController.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins =  "https://localhost:5173")
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/auth")
 public class AuthController {
 
